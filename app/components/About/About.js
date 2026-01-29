d8541bbd951a1ab8f94f884a4a1cabc3d827f157
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2026-01-29 11:54:40 +0100

    Fixing content

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -13,14 +13,15 @@ function About() {
     return (
         <div className="about" id="about" >
             <div className="line-styling">
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-line" style={{backgroundColor: theme.primary}}></div>
+                <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+                <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+                <div className="style-line" style={{backgroundColor: theme.primary}}></div>
             </div>
             <div className="about-body">
                 <div className="about-description">
                     <h2 style={{color: theme.primary}}>{aboutData.title}</h2>
-                    <p style={{color:theme.tertiary80}}>{aboutData.description1}<br/><br/>{aboutData.description2}</p>
+                    <p style={{color:theme.tertiary80}}>{aboutData.description1}<br/>
+                        <br/>{aboutData.description2}</p>
                 </div>
 
                 <div className="about-img">
--- app/data/aboutData.js
+++ app/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Qui-suis-je ?",
-    description1: "Bonjour, je suis Maxence Lagourgue, 20 ans, et actuellement Apprenti Ingénieur en Cybersécurité.",
+    description1: "Bonjour, et bienvenue dans mon portfolio, je suis Maxence Lagourgue, 20 ans, et j'occupe actuellement le poste d'Apprenti Ingénieur en Cybersécurité.",
     description2: " Dans la conjecture actuelle, la vie privée est plus que menacée, de par les fuites de données et les mécanismes de fichage ou de contrôle d'accès. En tant qu'ingénieur cybersécurité, l'éthique informatique doit faire réagir pour défendre ces libertés collectives et individuelles qui sont remises en question. L'Etat de droit est un principe auquel toute personne dans la cybersécurité doit être consciente, et doit préserver. C'est en suivant ces valeurs que je souhaite travailler dans la cybersécurité, en protégeant les SI pour permettre aux utilisateurs de ne pas crainde l'oppression, liées à leur opinion, religion, ou toute forme de discrimination.",
     image: 2
 }
